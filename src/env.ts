28a99b46e5c12822fddf65358c457de81c5fe288
Tomasz Kopacki <tomasz@kopacki.net>
2025-01-09 15:38:07 +0100

    diff: apply `linea.eth` changes

---
+++ src/env.ts
@@ -0,0 +1,4 @@
+export const BASE_DOMAIN = "linea.eth";
+export const BASE_NODE = "0x527aac89ac1d1de5dd84cff89ec92c69b028ce9ce3fa3d654882474ab4402ec3";
+export const BASE_LABEL = "linea";
+export const BASE_LABEL_HASH = "0x2e3dd66a6ec9a366e8b293f3a590467f043a76d049562752176edb48a3062d11";
--- src/nameWrapper.ts
+++ src/nameWrapper.ts
@@ -24,8 +24,9 @@ import {
   createEventID,
   createOrLoadAccount,
   createOrLoadDomain,
-  ETH_NODE,
 } from "./utils";
+
+import { BASE_NODE } from "./env";
 
 function decodeName(buf: Bytes): Array<string> | null {
   let offset = 0;
@@ -120,7 +121,7 @@ export function handleNameUnwrapped(event: NameUnwrappedEvent): void {
 
   let domain = createOrLoadDomain(node.toHex());
   domain.wrappedOwner = null;
-  if (domain.expiryDate && domain.parent != ETH_NODE) {
+  if (domain.expiryDate && domain.parent != BASE_NODE) {
     domain.expiryDate = null;
   }
   domain.save();
